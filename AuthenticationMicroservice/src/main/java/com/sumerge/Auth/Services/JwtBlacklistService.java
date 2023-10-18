18b3bcc214741d15cb2059f7c952e4f0cabce422
KhaledTamerAly <k.tamer155@gmail.com>
2023-10-18 22:55:28 +0300

    Todo: Tests

--- AuthenticationMicroservice/src/main/java/com/sumerge/Auth/Services/JwtBlacklistService.java
+++ AuthenticationMicroservice/src/main/java/com/sumerge/Auth/Services/JwtBlacklistService.java
@@ -3,14 +3,19 @@ package com.sumerge.Auth.Services;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class JwtBlacklistService {
     private ArrayList<String> blacklistedTokens;
+    private ScheduledExecutorService executorService;
 
     public JwtBlacklistService()
     {
         this.blacklistedTokens = new ArrayList<>();
+        this.executorService = Executors.newScheduledThreadPool(1);
     }
 
     public ArrayList<String> getBlacklistedTokens()
@@ -26,6 +31,7 @@ public class JwtBlacklistService {
     public void addToBlacklist(String token)
     {
         this.blacklistedTokens.add(token);
+        this.executorService.schedule(() -> this.blacklistedTokens.remove(token), 15, TimeUnit.MINUTES);
     }
 
     public boolean isBlacklisted(String token)
